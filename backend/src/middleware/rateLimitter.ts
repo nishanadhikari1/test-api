474e906bd8375f1043afed41b051e8262d90ecb4
nishanadhikari1 <adhikarin537@gmail.com>
2026-07-14 15:35:53 +0545

    rate limit update

--- backend/src/middleware/rateLimitter.ts
+++ backend/src/middleware/rateLimitter.ts
@@ -2,7 +2,7 @@ import rateLimit from 'express-rate-limit';
 
 export const authLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // limit each IP to 10 requests per window
+  max: 10, // limit each IP to 10 requests per window
   message: { error: 'Too many attempts, please try again later' },
   standardHeaders: true,
   legacyHeaders: false,
